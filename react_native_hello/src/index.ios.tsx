9c923a6fb3934a0b67b71452cfb5d0b52337f54c
lcc <leftcold@gmail.com>
2017-04-25 10:47:36 +0800

    添加ts支持

---
+++ react_native_hello/src/index.ios.tsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text, TextStyle,
+  View, ViewStyle
+} from 'react-native';
+
+interface Props {}
+
+interface State {}
+
+export default class App extends Component<Props, State> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit src/index.ios.tsx
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  } as ViewStyle,
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  } as TextStyle,
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  } as TextStyle,
+});
